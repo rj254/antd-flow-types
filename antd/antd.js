e5bc1c6a05a0f2bb5b12982cf77463032c36a621
Rishabh Jain <rishabh@theadsontop.com>
2017-10-06 14:41:19 -0700

    fix(flow): Fix flow exports

--- antd/antd.js
+++ antd/antd.js
@@ -1,23 +1,63 @@
 /* @flow */
 
-import { Menu, MenuProps, ClickParam, SelectParam } from './Menu';
-import { Spin, SpinProps } from './Spin';
-import { Col, ColProps } from './Col';
-import { Row, RowProps } from './Row';
-import { Card, CardProps } from './Card';
-import { Button, ButtonProps } from './Button/Button';
-import { Input, InputProps } from './Input/Input';
-import { Icon, IconProps } from './Icon';
-import { Form, FormProps } from './Form';
+import { Menu } from './Menu';
+import type {
+    MenuProps as IMenuProps,
+    ClickParam as IClickParam,
+    SelectParam as ISelectParam,
+} from './Menu';
+
+import { Spin } from './Spin';
+import type { SpinProps as ISpinProps } from './Spin';
+
+import { Col } from './Col';
+import type { ColProps as IColProps } from './Col';
+
+import { Row } from './Row';
+import type { RowProps as IRowProps } from './Row';
+
+import { Card } from './Card';
+import type { CardProps as ICardProps } from './Card';
+
+import { Button } from './Button/Button';
+import type { ButtonProps as IButtonProps } from './Button/Button';
+
+import { Input } from './Input/Input';
+import type { InputProps as IInputProps } from './Input/Input';
+
+import { Icon } from './Icon';
+import type { IconProps as IIconProps } from './Icon';
+
+import { Form } from './Form/Form';
+import type { FormProps as IFormProps } from './Form/Form';
 
 declare module 'antd' {
-    declare export { Menu, MenuProps, ClickParam, SelectParam };
-    declare export { Spin, SpinProps };
-    declare export { Col, ColProps };
-    declare export { Row, RowProps };
-    declare export { Card, CardProps };
-    declare export { Button, ButtonProps };
-    declare export { Input, InputProps };
-    declare export { Icon, IconProps };
-    declare export { Form, FormProps };
+    declare export { Menu };
+    declare type MenuProps = IMenuProps;
+    declare type ClickParam = IClickParam;
+    declare type SelectParam = ISelectParam;
+
+    declare export { Spin };
+    declare type SpinProps = ISpinProps;
+
+    declare export { Col };
+    declare type ColProps = IColProps;
+
+    declare export { Row };
+    declare type RowProps = IRowProps;
+
+    declare export { Card };
+    declare type CardProps = ICardProps;
+
+    declare export { Button };
+    declare type ButtonProps = IButtonProps;
+
+    declare export { Input };
+    declare type InputProps = IInputProps;
+
+    declare export { Icon };
+    declare type IconProps = IIconProps;
+
+    declare export { Form };
+    declare type FormProps = IFormProps;
 }
